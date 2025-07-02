528ce6a0a262c70f4df56d7e3c5fd3d68642be5e
Sagar Saha <Sagarsaha2579@gmail.com>
2025-07-01 21:18:10 -0400

    inital commit

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -3,8 +3,9 @@
     public class Christmas
     {
         public string[] Presents;
-        public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public string Santa {get; set;}
+
+    public int? TreeHeight { get;set; } //TODO Make the Height property nullable
 
         //TODO Make the property, "Day", type enum
         public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -11,22 +11,23 @@ namespace ChristmasWithTypes
             
             var xmasDay =  Christmas.Day.Thursday;
 
-            //TODO set Santa's name to Kris Kringle
+            //TODO set Santa's name to Kris Kringle0
             xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[]{}; 
+            xmas.Presents =  new string[] {"Xbox", "Nintendo Switch", "iPhone X"}; 
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
-            Console.WriteLine("Here are the presents we would like:");
+            Console.WriteLine("Here are the presents we would like: ");
 
             foreach (var present in xmas.Presents)
             {
-                Console.WriteLine($"    {present}");
+                Console.WriteLine($" {present}");
+              
             }
 
             Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
